fdbcc6e0286affafb64cdb25c8171a61ee98d825
Andrew Heinke <andrew.j.heinke@gmail.com>
2016-07-29 15:13:56 -0500

    added JawsDB connection info

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
 var mysql = require('mysql');
+
+// jawsDB connection
 var connection = mysql.createConnection({
 	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'cat_db'
+	host: 'izm96dhhnwr2ieg0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	user: 'nbi0twjkzqrmccj1',
+	password: 'tqhd5qyoo78rdtlt',
+	database: 'f2ogomvr39hea64m'
 });
 
 connection.connect(function (err) {
