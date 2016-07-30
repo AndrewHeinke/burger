94fb4ed2063413510c5dc5f262ca491f0afc0539
Andrew <andrew.j.heinke@gmail.com>
2016-07-29 21:56:49 -0500

    fully functional app

--- models/burger.js
+++ models/burger.js
@@ -1,20 +1,20 @@
 var orm = require('../config/orm.js');
 
 var burger = {
-	selectAll: function (cb) {
+	selectAll: function(cb) {
 		orm.selectAll('burgers', function (data) {
 			cb(data);
 		});
 	},
 
-	insertOne: function (col, burger_name, cb) {
-		orm.insertOne('burgers', col, burger_name, function (data) {
+	insertOne: function(col, burger_name, cb) {
+		orm.insertOne('burgers', col, burger_name, function(data) {
 			cb(data);
 		});
 	},
 
-	updateOne: function (col, burger_id, cb) {
-		orm.updateOne('burgers', col, burger_id, function (data) {
+	updateOne: function(col, burger_id, cb) {
+		orm.updateOne('burgers', col, burger_id, function(data) {
 			cb(data);
 		});
 	}
