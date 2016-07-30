28fa4ce847ece4047d56dc032da9263be8be1dde
Andrew Heinke <andrew.j.heinke@gmail.com>
2016-07-29 20:42:03 -0500

    adding jawsDB info

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,22 @@
 var mysql = require('mysql');
 
 // jawsDB connection
-// var connection = mysql.createConnection({
-// 	port: 3306,
-// 	host: 'izm96dhhnwr2ieg0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-// 	user: 'nbi0twjkzqrmccj1',
-// 	password: 'tqhd5qyoo78rdtlt',
-// 	database: 'f2ogomvr39hea64m'
-// });
-
 var connection = mysql.createConnection({
 	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'burgers_db'
+	host: 'izm96dhhnwr2ieg0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	user: 'nbi0twjkzqrmccj1',
+	password: 'tqhd5qyoo78rdtlt',
+	database: 'f2ogomvr39hea64m'
 });
 
+// local connection
+// var connection = mysql.createConnection({
+// 	port: 3306,
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: '',
+// 	database: 'burgers_db'
+// });
 
 connection.connect(function (err) {
 	if (err) {
